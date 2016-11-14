4dbf83f27e24ab2d6a3a8da359fee8ec97ec1d2d
pjbal <patrickjbalcombe@hotmail.com>
2016-11-14 14:52:22 +0000

    arduino obstacle avoidance robot basic code format

---
+++ ObsticalRobot_CodeFormat.ino
@@ -0,0 +1,53 @@
+const int leftControl1 = ;//set chosen pin value
+const int leftControl2 = ;//set chosen pin value
+const int leftEnable = ;//set chosen pin value
+
+const int rightControl1 = ;//set chosen pin value
+const int rightControl2 = ;//set chosen pin value
+const int rightEnable = ;//set chosen pin value
+
+const trigPin = ;//set chosen pin value
+const echoPin = ;//set chosen pin value
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //set up the up the input and output pins
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void forward(){
+  //write code to set the robot moving forward
+  
+}
+
+void backward(){
+  //write code to set the robot moving backward
+  
+}
+
+void stop_Moveing(){
+  //write code stop the robot
+  
+}
+
+void left(){
+  //write code to turn the robot 90 degrees to the left
+
+}
+
+
+void right(){
+  //write code to turn the robot 90 degrees to the right 
+
+}
+
+long getDistance(){
+  //write code to get the distance from the ultrasonic distance sensor
+}
+
+
